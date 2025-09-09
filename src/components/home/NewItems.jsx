f65db65d69ef02a8c00206efe6311bb22cd7da31
Keylan King <kingkey0101@outlook.com>
2025-09-09 18:37:55 -0500

    starting countdown logic

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
@@ -8,6 +8,39 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../../css/HotCollections.css";
+
+const useCountdown = (expiryDate) => {
+  const [timeLeft, setTimeLeft] = useState("");
+  const intervalRef = useRef(null);
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    isMounted.current = true;
+    if (!expiryDate) return;
+    const target = new Date(expiryDate).getTime();
+    intervalRef.current = setInterval(() => {
+      const now = new Date().getTime();
+      const diff = target - now;
+
+      if (diff < 0) {
+        if (isMounted.current) setTimeLeft("Expired");
+        clearInterval(intervalRef.current);
+        return;
+      }
+      const hours = Math.floor(diff / (1000 * 60 * 60));
+      const minutes = Math.floor((diff / (1000 * 60)) % 60);
+      const seconds = Math.floor((diff / 1000) % 60);
+      if (isMounted.current) {
+        setTimeLeft(`${hours}h ${minutes}m ${seconds}s`);
+      }
+    }, 1000);
+    return () => {
+      isMounted.current = false;
+      clearInterval(intervalRef.current);
+    };
+  }, [expiryDate]);
+  return timeLeft;
+};
 
 const NewItems = () => {
   const [data, setData] = useState(null);
@@ -103,67 +136,69 @@ const NewItems = () => {
             {data && (
               <div className="col-12 slider__container">
                 <Slider {...settings}>
-                  {data.slice(0, 6).map((item, index) => (
-                    <div key={index}>
-                      <div className="nft__item">
-                        <div className="author_list_pp">
-                          <Link
-                            to={`/author/${item.authorId}`}
-                            title={`Creator:${item.authorName}`}
-                          >
-                            <img
-                              className="lazy"
-                              src={item.authorImage || AuthorImage}
-                              alt=""
-                            />
-                            <i className="fa fa-check"></i>
-                          </Link>
-                        </div>
-
-                        {item.expiryDate && (
-                          <div className="de_countdown">{item.expiryDate}</div>
-                        )}
-
-                        <div className="nft__item_wrap">
-                          <div className="nft__item_extra">
-                            <div className="nft__item_buttons">
-                              <button>Buy Now</button>
-                              <div className="nft__item_share">
-                                <h4>Share</h4>
-                                <a href="" target="_blank" rel="noreferrer">
-                                  <i className="fa fa-facebook fa-lg"></i>
-                                </a>
-                                <a href="" target="_blank" rel="noreferrer">
-                                  <i className="fa fa-twitter fa-lg"></i>
-                                </a>
-                                <a href="">
-                                  <i className="fa fa-envelope fa-lg"></i>
-                                </a>
-                              </div>
-                            </div>
+                  {data.slice(0, 6).map((item, index) => {
+                    const timeLeft = useCountdown(item.expiryDate);
+                    return (
+                      <div key={index}>
+                        <div className="nft__item">
+                          <div className="author_list_pp">
+                            <Link
+                              to={`/author/${item.authorId}`}
+                              title={`Creator:${item.authorName}`}
+                            >
+                              <img
+                                className="lazy"
+                                src={item.authorImage || AuthorImage}
+                                alt=""
+                              />
+                              <i className="fa fa-check"></i>
+                            </Link>
                           </div>
 
-                          <Link to={`/item-details/${item.nftId}`}>
-                            <img
-                              src={item.nftImage || nftImage}
-                              className="lazy nft__item_preview"
-                              alt={item.title}
-                            />
-                          </Link>
-                        </div>
-                        <div className="nft__item_info">
-                          <Link to={`/item-details/${item.nftId}`}>
-                            <h4>{item.title || "Untitled"}</h4>
-                          </Link>
-                          <div className="nft__item_price">{item.price}</div>
-                          <div className="nft__item_like">
-                            <i className="fa fa-heart"></i>
-                            <span>{item.likes}</span>
+                          {item.expiryDate && (
+                            <div className="de_countdown">{timeLeft}</div>
+                          )}
+                          <div className="nft__item_wrap">
+                            <div className="nft__item_extra">
+                              <div className="nft__item_buttons">
+                                <button>Buy Now</button>
+                                <div className="nft__item_share">
+                                  <h4>Share</h4>
+                                  <a href="" target="_blank" rel="noreferrer">
+                                    <i className="fa fa-facebook fa-lg"></i>
+                                  </a>
+                                  <a href="" target="_blank" rel="noreferrer">
+                                    <i className="fa fa-twitter fa-lg"></i>
+                                  </a>
+                                  <a href="">
+                                    <i className="fa fa-envelope fa-lg"></i>
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+
+                            <Link to={`/item-details/${item.nftId}`}>
+                              <img
+                                src={item.nftImage || nftImage}
+                                className="lazy nft__item_preview"
+                                alt={item.title}
+                              />
+                            </Link>
+                          </div>
+                          <div className="nft__item_info">
+                            <Link to={`/item-details/${item.nftId}`}>
+                              <h4>{item.title || "Untitled"}</h4>
+                            </Link>
+                            <div className="nft__item_price">{item.price}</div>
+                            <div className="nft__item_like">
+                              <i className="fa fa-heart"></i>
+                              <span>{item.likes}</span>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  ))}
+                    );
+                  })}
                 </Slider>
               </div>
             )}
