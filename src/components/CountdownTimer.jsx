8b1201baf4105d3a08d8e7e0391ad333dfe2b77b
Keylan King <kingkey0101@outlook.com>
2025-09-09 18:42:56 -0500

    countdown timer logic

--- src/components/CountdownTimer.jsx
+++ src/components/CountdownTimer.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-const CountdownTimer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const CountdownTimer = ({expiryDate}) => {
+    const [timeLeft, setTimeLeft] = useState('');
+    const intervalRef = useRef(null);
+    const isMounted = useRef(true);
+
+    useEffect(()=>{
+        isMounted.current = true;
+        if((!expiryDate)) return;
+
+        const target = new Date(expiryDate).getTime();
+
+        intervalRef.current = setInterval(() => {
+            
+        }, ((interval=>{
+            const now = new Date().getTime();
+            const diff = target - now;
+            
+        })));
+    })
 }
 
 export default CountdownTimer
