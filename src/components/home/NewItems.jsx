22e7079204b09ec511fd03571fe372a2abdcbde3
Keylan King <kingkey0101@outlook.com>
2025-09-09 14:34:30 -0500

    First commit to NewItems.jsx - used axios for API, printed authorImage, nftImage,title,

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
+import Slider from "react-slick";
 
 const NewItems = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: true,
+    responsive: [
+      { breakpoint: 1024, settings: { slidesToShow: 3 } },
+      { breakpoint: 768, settings: { slidesToShow: 2 } },
+      { breakpoint: 480, settings: { slidesToShow: 1 } },
+    ],
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems"
+      )
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <section id="section-items" className="no-bottom">
+        <div className="container">
+          <div className="row">
+            <Slider {...settings}>
+
+            {[...Array(7)].map((_, index) => (
+              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                <Skeleton width="100%" height="300px" borderRadius="12px" />;
+              </div>
+            ))}
+            </Slider>
+          </div>
+        </div>
+      </section>
+    );
+  }
+  if (error) return <p>Error: {error.message}</p>;
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,21 +69,27 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
+          {data.map((item, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
+                    to={`/author/${item.authorId}`}
+                    title={`Creator:${item.authorName}`}
                   >
-                    <img className="lazy" src={AuthorImage} alt="" />
+                    <img
+                      className="lazy"
+                      src={item.authorImage || AuthorImage}
+                      alt=""
+                    />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
-                <div className="de_countdown">5h 30m 32s</div>
+
+                {item.expiryDate && (
+                  <div className="de_countdown">{item.expiryDate}</div>
+                )}
+                {/* <div className="de_countdown">5h 30m 32s</div> */}
 
                 <div className="nft__item_wrap">
                   <div className="nft__item_extra">
@@ -49,22 +110,22 @@ const NewItems = () => {
                     </div>
                   </div>
 
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${item.nftId}`}>
                     <img
-                      src={nftImage}
+                      src={item.nftImage || nftImage}
                       className="lazy nft__item_preview"
-                      alt=""
+                      alt={item.title}
                     />
                   </Link>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                  <Link to={`/item-details/${item.nftId}`}>
+                    <h4>{item.title || "Untitled"}</h4>
                   </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
+                  <div className="nft__item_price">{item.price}</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>69</span>
+                    <span>{item.likes}</span>
                   </div>
                 </div>
               </div>
