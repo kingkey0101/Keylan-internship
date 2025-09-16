1ed3cfcbadc9ad75c3f35569e5ea4d2f769944bf
Keylan King <kingkey0101@outlook.com>
2025-09-15 19:21:27 -0500

    fixing spacing

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -37,7 +37,9 @@ const ItemDetails = () => {
         <div className="container">
           <div className="row">
             <div className="col-md-6 text-center">
+
               {/* skeleton img */}
+
               <Skeleton width="100%" height="300px" borderRadius="8px" />
             </div>
             {/* skeleton details */}
@@ -46,28 +48,33 @@ const ItemDetails = () => {
               <div className="item_info">
                 <Skeleton width="60%" height="32px" />
                 {/* skeleton views and likes */}
+
                 <div className="d-flex align-items-center mt-2 mb-3">
                   <Skeleton width="40px" height="20px" className="mb-4" />
                   <Skeleton width="40px" height="20px" />
                 </div>
 
                 {/* skeleton description */}
+
                 <Skeleton width="100%" height="16px" className="mb-1" />
                 <Skeleton width="100%" height="16px" className="mb-1" />
                 <Skeleton width="80%" height="16px" />
                 {/* skeleton owner */}
+
                 <div className="d-flex align-items-center mb-3">
                   <Skeleton width="50px" height="50px" />
                   <Skeleton width="120px" height="16px" className="ms-3" />
                 </div>
 
                 {/* skeleton creator */}
+
                 <div className="d-flex align-items-center mb-3">
                   <Skeleton width="50%" height="50px" borderRadius="50%" />
                   <Skeleton width="120px" height="16px" className="mt-3" />
                 </div>
 
                 {/* skeleton price */}
+
                 <Skeleton width="100px" height="32px" />
               </div>
             </div>
@@ -109,28 +116,38 @@ const ItemDetails = () => {
               </div>
               <div className="col-md-6">
                 <div className="item_info">
+
                   {/* title */}
+
                   <h2>{title}</h2>
                   <div className="item_info_counts">
                     <div className="item_info_views">
+
                       {/* views */}
+
                       <i className="fa fa-eye"></i>
                       {views}
                     </div>
                     <div className="item_info_like">
+
                       {/* likes */}
+
                       <i className="fa fa-heart"></i>
                       {likes}
                     </div>
                   </div>
+
                   {/* description */}
+
                   <p>{description}</p>
                   <div className="d-flex flex-row">
                     <div className="mr40">
                       <h6>Owner</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
+
                           {/* owner image  */}
+
                           <Link to="/author">
                             <img
                               className="lazy"
@@ -141,7 +158,9 @@ const ItemDetails = () => {
                           </Link>
                         </div>
                         <div className="author_list_info">
+
                           {/* owner name */}
+
                           <Link to="/author">{ownerName}</Link>
                         </div>
                       </div>
@@ -153,20 +172,26 @@ const ItemDetails = () => {
                       <h6>Creator</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
+
                           {/* creator image */}
+
                           <Link to="/author">
                             <img className="lazy" src={creatorImage} alt="" />
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
+
                           {/* creator name */}
+
                           <Link to="/author">{creatorName}</Link>
                         </div>
                       </div>
                     </div>
                     <div className="spacer-40"></div>
+
                     {/* price */}
+                    
                     <h6>Price</h6>
                     <div className="nft-item-price">
                       <img src={EthImage} alt="" />
