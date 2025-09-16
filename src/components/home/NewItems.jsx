ecd8b7ca7cdfa6360811c78a6e5229ba6e3d2d49
Keylan King <kingkey0101@outlook.com>
2025-09-16 14:26:12 -0500

    added animation to NewItems.jsx

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -58,7 +58,7 @@ const NewItems = () => {
     return (
       <section id="section-items" className="no-bottom">
         <div className="container">
-          <div className="row">
+          <div data-aos="fade-in" className="row">
             <div className="col-lg-12">
               <div className="text-center">
                 <h2>New Items</h2>
@@ -117,7 +117,7 @@ const NewItems = () => {
     <>
       <section id="section-items" className="no-bottom">
         <div className="container">
-          <div className="row">
+          <div data-aos="fade-in" className="row">
             <div className="col-lg-12 text-center">
               <h2>New Items</h2>
               <div className="small-border bg-color-2"></div>
